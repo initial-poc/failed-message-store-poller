da3642a8be9dfc13b7bc2d17177115b2b1135799
initial-poc <gupta.hemanshu@gmail.com>
2021-05-02 14:49:49 +0530

    removed unnecessary files

--- src/main/java/com/infogain/gcp/poc/component/FailedMessageGroupRecordPollerExector.java
+++ src/main/java/com/infogain/gcp/poc/component/FailedMessageGroupRecordPollerExector.java
@@ -1,14 +1,10 @@
 package com.infogain.gcp.poc.component;
 
 import com.infogain.gcp.poc.poller.service.MessageGroupRecordProcessorService;
-import com.infogain.gcp.poc.poller.service.OutboxRecordProcessorService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalTime;
 
--- src/main/java/com/infogain/gcp/poc/poller/repository/SpannerOutboxRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infogain.gcp.poc.poller.repository;
-
-import org.springframework.cloud.gcp.data.spanner.repository.SpannerRepository;
-
-import com.infogain.gcp.poc.poller.entity.OutboxEntity;
-
-public interface SpannerOutboxRepository extends SpannerRepository<OutboxEntity	, String> { }
